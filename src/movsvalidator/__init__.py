7ef935656cafad5cc8407b7390f33b85e6065c8e
Vito De Tullio <vito.detullio@finconsgroup.com>
2020-04-05 12:24:06 +0200

    working

--- src/movsvalidator/__init__.py
+++ src/movsvalidator/__init__.py
@@ -3,13 +3,16 @@ import movs
 
 class Validator:
     def __init__(self, fn: str) -> None:
+        print(fn)
         kv, csv = movs.read_txt(fn)
         self.kv = kv
         self.csv = csv
 
     def validate(self):
-        print(f'bpol.saldo_contabile: {self.kv.saldo_contabile}')
-        print(f'bpol.saldo_disponibile: {self.kv.saldo_disponibile}')
+        print(f'bpol.saldo_contabile:               {self.kv.saldo_contabile}')
+        print(f'bpol.saldo_disponibile:             {self.kv.saldo_disponibile}')
 
-        s = sum(item.accredito - item.addebito for item in self.csv)
+        s = sum((item.accrediti if item.accrediti is not None else 0) -
+                (item.addebiti if item.addebiti is not None else 0)
+                for item in self.csv)
         print(f'Σ (item.accredito - item.addebito): {s}')
