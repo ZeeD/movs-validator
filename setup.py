6138e56b5227ef96a3fd0251f2c9bc976816bfe8
Vito De Tullio <vito.detullio@gmail.com>
2021-03-31 11:47:37 +0200

    imports

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
-import glob
-import os
-import typing
+from setuptools import find_packages
+from setuptools import setup
 
-import setuptools
-
-setuptools.setup(name='movs-validator',
-                 version='0.0.1',
-                 author='Vito De Tullio',
-                 author_email='vito.detullio@gmail.com',
-                 description='movs validator',
-                 url='https://github.com/ZeeD/movs-validator',
-                 packages=setuptools.find_packages(),
-                 python_requires='>=3.8',
-                 package_dir={'': 'src'},
-                 install_requires=[
-                     'movs'
-                 ],
-                 package_data={
-                     'movsvalidator': ['py.typed'],
-                 },
-                 entry_points={
-                     'console_scripts': [
-                         'movs-validator = movsvalidator:main'
-                     ]
-                 })
+setup(name='movs-validator',
+      version='0.0.1',
+      author='Vito De Tullio',
+      author_email='vito.detullio@gmail.com',
+      description='movs validator',
+      url='https://github.com/ZeeD/movs-validator',
+      packages=find_packages(),
+      python_requires='>=3.8',
+      package_dir={'': 'src'},
+      install_requires=[
+          'movs'
+      ],
+      package_data={
+          'movsvalidator': ['py.typed'],
+      },
+      entry_points={
+          'console_scripts': [
+              'movs-validator = movsvalidator:main'
+          ]
+      })
