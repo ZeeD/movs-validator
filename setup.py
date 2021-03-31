1ea21930ebf4f25eecb9bb54291acd6e06eae849
Vito De Tullio <vito.detullio@gmail.com>
2021-03-31 11:43:01 +0200

    refactor

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import typing
 
 import setuptools
 
-
 setuptools.setup(name='movs-validator',
                  version='0.0.1',
                  author='Vito De Tullio',
@@ -13,6 +12,15 @@ setuptools.setup(name='movs-validator',
                  url='https://github.com/ZeeD/movs-validator',
                  packages=setuptools.find_packages(),
                  python_requires='>=3.8',
+                 package_dir={'': 'src'},
                  install_requires=[
                      'movs'
-                 ])
+                 ],
+                 package_data={
+                     'movsvalidator': ['py.typed'],
+                 },
+                 entry_points={
+                     'console_scripts': [
+                         'movs-validator = movsvalidator:main'
+                     ]
+                 })
--- src/movsvalidator/__init__.py
+++ src/movsvalidator/__init__.py
@@ -1,29 +1,39 @@
-import datetime
-import decimal
+from datetime import date
+from decimal import Decimal
 
-import movs
+from movs import read_txt
+
+ZERO = Decimal(0)
 
 
-class Validator:
-    def __init__(self, fn: str) -> None:
-        print(fn)
-        kv, csv = movs.read_txt(fn)
-        self.kv = kv
-        self.csv = csv
+def validate(fn: str) -> None:
+    print(fn)
+    kv, csv = read_txt(fn)
+    print(f'bpol.saldo_al:                      {kv.saldo_al}')
+    if kv.saldo_al:
+        ultimo_update = (date.today() - kv.saldo_al).days
+        print(f'ultimo update:                      {ultimo_update} giorni fa')
+    print(f'bpol.saldo_contabile:               {kv.saldo_contabile}')
+    print(f'bpol.saldo_disponibile:             {kv.saldo_disponibile}')
 
-    def validate(self) -> None:
-        print(f'bpol.saldo_al:                      {self.kv.saldo_al}')
-        if self.kv.saldo_al:
-            ultimo_update = (datetime.date.today() - self.kv.saldo_al).days
-            print(
-                f'ultimo update:                      {ultimo_update} giorni fa')
-        print(f'bpol.saldo_contabile:               {self.kv.saldo_contabile}')
-        print(
-            f'bpol.saldo_disponibile:             {self.kv.saldo_disponibile}')
+    s = sum((item.accrediti if item.accrediti is not None else ZERO) -
+            (item.addebiti if item.addebiti is not None else ZERO)
+            for item in csv)
+    print(f'Σ (item.accredito - item.addebito): {s}')
 
-        zero = decimal.Decimal(0)
 
-        s = sum((item.accrediti if item.accrediti is not None else zero) -
-                (item.addebiti if item.addebiti is not None else zero)
-                for item in self.csv)
-        print(f'Σ (item.accredito - item.addebito): {s}')
+from sys import argv
+
+
+def main() -> None:
+    'main'
+
+    if not argv[1:]:
+        raise SystemExit(f'uso: {argv[0]} ACCUMULATOR...')
+
+    for fn in argv[1:]:
+        validate(fn)
+
+
+if __name__ == '__main__':
+    main()
