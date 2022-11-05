2f81d7011c22d95277cc53d54ab65ef4d21a6763
Vito De Tullio <vito.detullio@gmail.com>
2022-11-05 10:45:22 +0100

    refactor

--- README.md
+++
@@ -1,3 +0,0 @@
-# movs-validator
-
-![Code scanning - action](https://github.com/ZeeD/movs-validator/workflows/Code%20scanning%20-%20action/badge.svg)
--- setup.py
+++ setup.py
@@ -2,14 +2,11 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name='movs-validator',
-      version='0.0.1',
+      version='0.0.0',
+      url='https://github.com/ZeeD/movs-validator',
       author='Vito De Tullio',
       author_email='vito.detullio@gmail.com',
-      description='movs validator',
-      url='https://github.com/ZeeD/movs-validator',
       packages=find_packages(),
-      python_requires='>=3.8',
-      package_dir={'': 'src'},
       install_requires=[
           'movs'
       ],
